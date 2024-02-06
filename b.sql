a75bfab409ee5b8109332b0871d410557ca4990c
neojarvis <techadmin@iamneo.ai>
2024-02-06 08:10:23 +0000

    Auto-save-commit-2/6/2024--1:40:18-PM

---
+++ b.sql
@@ -0,0 +1,11 @@
+    -- Create the table
+    CREATE TABLE Example2 (
+        ID int,
+        Name nvarchar(255)
+    );
+
+    -- Insert values into the table
+    INSERT INTO Example1 (ID, Name) VALUES (1, 'ram');
+    INSERT INTO Example1 (ID, Name) VALUES (2, 'chandru');
+
+-- Select and view all data from the tabl
